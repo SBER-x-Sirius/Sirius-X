cd7ee9bcd5ecd44e2d90232f9be60f72ec163bc5
Naziser <you@example.com>
2023-09-21 01:24:33 +0300

    feat: add base-header

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import Main from './pages/main';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Attendance from './pages/attendance';
-import Schedule from './pages/schedule';
-import Statistics from './pages/statistics';
-import Header from './components/header';
+import React from 'react'
+import Main from './pages/main'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Attendance from './pages/attendance'
+import Schedule from './pages/schedule'
+import Statistics from './pages/statistics'
+import Header from './components/header'
 
 export const BASE_PATH = '/sirius-x'
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path={BASE_PATH} element={<Main />} />
-        <Route path={`${BASE_PATH}/attendance`} element={<Attendance />} />
-        <Route path={`${BASE_PATH}/schedule`} element={<Schedule />} />
-        <Route path={`${BASE_PATH}/statistics`} element={<Statistics />} />
-      </Routes>
-    </BrowserRouter>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path={BASE_PATH} element={<Main />} />
+                <Route path={`${BASE_PATH}/attendance`} element={<Attendance />} />
+                <Route path={`${BASE_PATH}/schedule`} element={<Schedule />} />
+                <Route path={`${BASE_PATH}/statistics`} element={<Statistics />} />
+            </Routes>
+        </BrowserRouter>
   )
-};
+}
 
-export default App;
+export default App
