cc4f5e06cf0a3941f2699d46cf011369359471fd
kuzy53 <nikita129343@mail.ru>
2023-09-26 16:15:06 +0300

    Chart edit

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useTheme } from '@mui/material';
 import {
     PageContent
 } from "./styles";
 import WelcomeCard from "./welcomeCard/index";
-const Statistics = () => {
+import DoughnutChart from "./eChart/index";
+
+const Statistics = (): JSX.Element => {
+  const theme = useTheme();
   return (
       <PageContent>
         <WelcomeCard/>
+        <DoughnutChart
+          height="350px"
+          color={[
+            theme.palette.primary.dark,
+            theme.palette.primary.main,
+            theme.palette.primary.light
+          ]}
+        />
       </PageContent>
   );
 };
