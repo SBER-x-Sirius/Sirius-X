c75b0f469475ce10aa9e9f720d583ae83e8731ce
Naziser <you@example.com>
2023-09-28 12:57:01 +0300

    feat: session page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,6 +7,7 @@ import {
   HeaderItemsWrapper
 } from './styles'
 import { getNavigationsValue } from '@ijl/cli'
+import LogOutSVG from '../../assets/svg/log-out.svg';
 
 type Link = {
     text: string;
@@ -44,33 +45,31 @@ const Header: React.FC = (): JSX.Element => {
   ))
 
   return (
+    <>
         <ContainerHeader>
-            <MenuNavigation>
-                <MenuList>
-                    {linkElements}
-                </MenuList>
-            </MenuNavigation>
-            <HeaderItemsWrapper>
-                <LocalizeWrapper>
-                    <LocalizeButton
-                        onClick={ () => handleLocalizeClick('ru') }
-                        active={ activeLocalizeBtn === 'ru' }
-                    >ru</LocalizeButton>
-                    <LocalizeButton
-                        onClick={ () => handleLocalizeClick('en') }
-                        active={ activeLocalizeBtn === 'en' }
-                    >en</LocalizeButton>
-                </LocalizeWrapper>
-                <ProfileLink to={'#'}>
-                    Личный кабинет
-                </ProfileLink>
-                <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M13.9412 17.6667L19.5 11.8333L13.9412 6" stroke="#343F42" strokeLinecap="round" strokeLinejoin="round"/>
-                    <path d="M18.7059 11.8334H6" stroke="#343F42" strokeLinecap="round" strokeLinejoin="round"/>
-                    <path d="M15.8581 1L3.97163 1.00353C2.33077 1.0049 1.00083 2.41214 1 4.1479V19.8565C1 21.5926 2.33044 23 3.97163 23H16" stroke="#343F42" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-            </HeaderItemsWrapper>
-        </ContainerHeader>
+                <MenuNavigation>
+                    <MenuList>
+                        {linkElements}
+                    </MenuList>
+                </MenuNavigation>
+                <HeaderItemsWrapper>
+                    <LocalizeWrapper>
+                        <LocalizeButton
+                            onClick={ () => handleLocalizeClick('ru') }
+                            active={ activeLocalizeBtn === 'ru' }
+                        >ru</LocalizeButton>
+                        <LocalizeButton
+                            onClick={ () => handleLocalizeClick('en') }
+                            active={ activeLocalizeBtn === 'en' }
+                        >en</LocalizeButton>
+                    </LocalizeWrapper>
+                    <ProfileLink to={'#'}>
+                        Личный кабинет
+                    </ProfileLink>
+                    <img src={LogOutSVG} alt="Кнопка выхода из личного кабинета" />
+                </HeaderItemsWrapper>
+            </ContainerHeader>
+    </>
   )
 }
 
