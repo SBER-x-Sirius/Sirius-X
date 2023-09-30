a9779ce8964b37fa75c6e8125c4ebfa983828f28
Daniel <91885191+DanielUsov@users.noreply.github.com>
2023-09-30 12:05:08 +0300

    feat: api stub (#23)
    Co-authored-by: Daniel <dan.usov82@gmail.com>

--- src/app.tsx
+++ src/app.tsx
@@ -6,10 +6,13 @@ import { Schedule } from './pages/schedule';
 import Statistics from './pages/statistics';
 import Header from './components/header';
 import { GlobalStyle } from '../global-styled';
+import { useGetAllUsersQuery } from './store/api/attendance/user';
 
 export const BASE_PATH = '/sirius-x';
 
 const App = () => {
+  const { data } = useGetAllUsersQuery();
+
   return (
     <BrowserRouter>
       <GlobalStyle />
@@ -20,6 +23,7 @@ const App = () => {
         <Route path={`${BASE_PATH}/schedule`} element={<Schedule />} />
         <Route path={`${BASE_PATH}/statistics`} element={<Statistics />} />
       </Routes>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
     </BrowserRouter>
   );
 };
