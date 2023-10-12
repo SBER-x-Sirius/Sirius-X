80f3d5d9c82484331fc011afbdd13b8d0a5229ec
kuzy53 <nikita129343@mail.ru>
2023-10-12 22:00:46 +0300

    LineChert edit

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import SpecialtyBlock from './specialtyBlock';
-const Statistics = () => {
+import { useTheme } from '@mui/material';
+import LineChart from './lineEchart';
+const Statistics = (): JSX.Element => {
+  const theme = useTheme();
   return (
+    <>
       <SpecialtyBlock/>
+      <LineChart
+          height="350px"
+          color={[theme.palette.primary.main, theme.palette.primary.light]}
+        />
+      </>
 );
 };
 
