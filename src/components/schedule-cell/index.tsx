fcd86974e009e6ddd9c8bf8c0d60400576ca221e
Artur Amantaev <aa@megaperfume.ru>
2023-10-21 13:39:01 +0300

    sidebar update
    i18n
    
    fixed
    
    fixed
    
    fixed

--- src/components/schedule-cell/index.tsx
+++ src/components/schedule-cell/index.tsx
@@ -2,40 +2,38 @@ import React from 'react';
 import { ScheduleItem, Time, Name, Place, Teacher, MainBlock, PlaceText, Header, Number } from './styles';
 import Map from '../../assets/svg/map.svg';
 import Info from '../../assets/svg/info.svg';
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 type propsScheduleCell = {
-    data: {
-        lessonKey: string,
-        name: string,
-        placeActivity: string,
-        teacher: string,
-        time: string,
-    }
+  data: {
+    lessonKey: string;
+    name: string;
+    placeActivity: string;
+    teacher: string;
+    time: string;
+  };
 };
 
+const ScheduleCell = ({ data }: propsScheduleCell): JSX.Element => {
+  const { t } = useTranslation();
 
-const ScheduleCell = ({data}: propsScheduleCell): JSX.Element => {
-
-    const { t } = useTranslation();
-
-    return (
-        <ScheduleItem>
-            <MainBlock>
-                <Header>
-                    <Number>{data.lessonKey}</Number>
-                    <Time>{data.time}</Time>
-                    <img src={Info} alt={t('schedule:scheduleTranslation.scheduleGrid.infoIcon')} />
-                </Header>
-                <Name>{data.name}</Name>
-                <Place>
-                    <img src={Map} alt={t('schedule:scheduleTranslation.scheduleGrid.mapIcon')} />
-                    <PlaceText>{data.placeActivity}</PlaceText>
-                </Place>
-                <Teacher>{data.teacher}</Teacher>
-            </MainBlock>
-        </ScheduleItem>
-    );
+  return (
+    <ScheduleItem>
+      <MainBlock>
+        <Header>
+          <Number>{data.lessonKey}</Number>
+          <Time>{data.time}</Time>
+          <img src={Info} alt={t('schedule:scheduleTranslation.scheduleGrid.infoIcon')} />
+        </Header>
+        <Name>{data.name}</Name>
+        <Place>
+          <img src={Map} alt={t('schedule:scheduleTranslation.scheduleGrid.mapIcon')} />
+          <PlaceText>{data.placeActivity}</PlaceText>
+        </Place>
+        <Teacher>{data.teacher}</Teacher>
+      </MainBlock>
+    </ScheduleItem>
+  );
 };
 
 export default ScheduleCell;
