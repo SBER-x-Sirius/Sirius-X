f5979cd87ae45b62d43f68ed49994fc996fa40b5
Naziser <SeregaSwapp@mail.ru>
2023-10-29 14:22:33 +0300

    feat: session page localize

--- src/pages/attendance/users/index.tsx
+++ src/pages/attendance/users/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 import Sidebar from '../../../components/sidebar';
-import { UsersPageWrapper, UserPageHeader, GroupsBlock, GroupsList, ShareButton, SettingsIcon, ButtonsBlock, UserPageTableWrapper, UserPageTable, TableRow, TableCell, TableImageCell, TableIcon } from './style';
+import { UsersPageWrapper, UserPageHeader, GroupsBlock, GroupsList,
+ShareButton, SettingsIcon, ButtonsBlock, UserPageTableWrapper,
+UserPageTable, TableRow, TableCell, TableImageCell,
+TableIcon } from './style';
 import SettingsSVG from '../../../assets/svg/settings_24px.svg';
 import TableSVG from '../../../assets/svg/Group 71.svg';
 
-export const Users = () => {
-  const groups = ['К0711-21/1', 'К0711-21/2', 'К0711-21/3'];
+type User = {
+  initials: string;
+  group: string;
+  role: string;
+};
 
-  const users = [
+export const Users: React.FC = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  const groups: string[] = ['К0711-21/1', 'К0711-21/2', 'К0711-21/3'];
+
+  const users: User[] = [
     { initials: 'Рындин Сергей Иванович', group: 'К0711-21/2', role: 'Студент'},
     { initials: 'Усов Даниил Андреевич', group: 'К0711-21/2', role: 'Студент'},
     { initials: 'Амантаев Артур Махмудович', group: 'К0711-21/2', role: 'Студент'},
@@ -19,13 +32,13 @@ export const Users = () => {
       <UserPageHeader>
         <GroupsBlock>
         <GroupsList>
-          Присутствующие группы: {groups.join(', ')}
+          {t('attendance:attendanceTranslation.session-page.groupList')}: {groups.join(', ')}
         </GroupsList>
         </GroupsBlock>
 
         <ButtonsBlock>
-          <ShareButton>Поделиться</ShareButton>
-          <SettingsIcon src={SettingsSVG} alt='Кнопка настроек'/>
+          <ShareButton>{t('attendance:attendanceTranslation.session-page.shareButton')}</ShareButton>
+          <SettingsIcon src={SettingsSVG} alt={t('attendance:attendanceTranslation.session-page.shareButtonAlt')}/>
         </ButtonsBlock>
       </UserPageHeader>
       <UserPageTableWrapper>
@@ -37,7 +50,7 @@ export const Users = () => {
                 <TableCell>{user.group}</TableCell>
                 <TableCell>{user.role}</TableCell>
                 <TableImageCell>
-                  <TableIcon src={TableSVG} alt='Кнопка дополнительных действий'/>
+                  <TableIcon src={TableSVG} alt={t('attendance:attendanceTranslation.session-page.TableIconAlt')}/>
                 </TableImageCell>
               </TableRow>
             ))}
