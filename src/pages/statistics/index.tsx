06483dbd16dd33cc8f3dc74d1a539a213d5a694b
oleg-yesterdays <olegyesterdays@gmail.com>
2023-11-12 21:05:35 +0300

    welcomeCard fix

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import SpecialtyBlock from './specialtyBlock';
-import { useTheme } from '@mui/material';
+import {useTheme} from '@mui/material';
 import LineChart from './lineChart';
+import WelcomeCard from './welcomeCard/index';
 
 const Statistics = (): JSX.Element => {
-  const theme = useTheme();
-  return (
-    <>
-      <SpecialtyBlock/>
-      <LineChart
-          height="350px"
-          color={[theme.palette.primary.main, theme.palette.primary.light]}
-        />
-      </>
-);
+    const theme = useTheme();
+    return (
+        <>
+            <WelcomeCard/>
+            <SpecialtyBlock/>
+            <LineChart
+                height="350px"
+                color={[theme.palette.primary.main, theme.palette.primary.light]}
+            />
+        </>
+    );
 };
 
 export default Statistics;
