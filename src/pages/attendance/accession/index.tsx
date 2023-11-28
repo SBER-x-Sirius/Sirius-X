972dea8e131cc102f220e18d86f8ffb251b46cc5
Amantaev-Artur <amantaev_artur@mail.ru>
2023-11-28 13:58:17 +0300

    fixed

--- src/pages/attendance/accession/index.tsx
+++ src/pages/attendance/accession/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Sidebar from '../../../components/sidebar';
 import BaseForm from '../../../components/form';
 import { AttendanceWrapper } from '../style';
 import { SessionButton, SessionFormContent, SessionInput } from './style';
 import { useAccessionMutation } from '../../../__data__/services/api/attendance/accession';
-import { Alert, Slide } from '@mui/material';
+import { Alert, Slide, SlideProps } from '@mui/material';
 import Snackbar from '@mui/material/Snackbar';
 import { TransitionProps } from 'react-transition-group/Transition';
 
@@ -19,22 +19,26 @@ const Accession: React.FC = (): JSX.Element => {
   const { t } = useTranslation();
 
   const handleMeetingId = (event): void => {
+    setShowErrorPopup(false);
     setMeetingId(event.target.value);
   };
-  function TransitionUp(props: TransitionProps) {
+  function TransitionUp(props: SlideProps) {
     return <Slide {...props} direction='up' />;
   }
+
+  useEffect(() => {
+    if (attendanceAccessionErrors) {
+      setShowErrorPopup(true);
+    } else if (attendanceAccessionData) {
+      setShowCompletedPopup(true);
+    }
+  }, [attendanceAccessionData, attendanceAccessionErrors]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (meetingId) {
       await attendanceAccession({ meetingId: meetingId ? meetingId : '' });
-      if (attendanceAccessionErrors) {
-        setShowErrorPopup(true);
-      } else {
-        setShowCompletedPopup(true);
-      }
     }
   };
 
@@ -49,29 +53,27 @@ const Accession: React.FC = (): JSX.Element => {
             placeholder={t('attendance:attendanceTranslation.sessionForm.inputPlaceholder')}
             disabled={showCompletedPopup}
           />
-          <SessionButton onClick={(e) => handleSubmit(e)}
-                      disabled={showCompletedPopup}
-                      >
+          <SessionButton onClick={(e) => handleSubmit(e)} disabled={showCompletedPopup}>
             {t('attendance:attendanceTranslation.sessionForm.btnText')}
           </SessionButton>
         </SessionFormContent>
       </BaseForm>
       {(showCompletedPopup || showErrorPopup) && (
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center'
-        }}
-        TransitionComponent={TransitionUp}
-        open={showCompletedPopup || showErrorPopup}
-        autoHideDuration={6000}
-      >
-        <Alert severity={showCompletedPopup ? 'success' : 'error'} sx={{ zoom: '1.3' }}>
-          {showCompletedPopup
-            ? t('attendance:attendanceTranslation.sessionForm.completed')
-            : t('attendance:attendanceTranslation.sessionForm.error')}
-        </Alert>
-      </Snackbar>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center'
+          }}
+          TransitionComponent={TransitionUp}
+          open={showCompletedPopup || showErrorPopup}
+          autoHideDuration={6000}
+        >
+          <Alert severity={showCompletedPopup ? 'success' : 'error'} sx={{ zoom: '1.3' }}>
+            {showCompletedPopup
+              ? t('attendance:attendanceTranslation.sessionForm.completed')
+              : t('attendance:attendanceTranslation.sessionForm.error')}
+          </Alert>
+        </Snackbar>
       )}
     </AttendanceWrapper>
   );
