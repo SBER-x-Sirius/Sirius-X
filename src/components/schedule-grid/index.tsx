7b69ce2ee0f8e23e24e6589327a00c5e11fb2a20
Limonadek <den-stepanov-03@mail.ru>
2024-01-08 18:19:05 +0300

    fix-router, add toggle week a schedule and add loader fetch schedule

--- src/components/schedule-grid/index.tsx
+++ src/components/schedule-grid/index.tsx
@@ -1,28 +1,65 @@
-import React from 'react';
-import { ScheduleContainer, ChooseWeek, Whitespace, ScheduleWrapper, ScheduleError } from './styles';
+import React, { useState } from 'react';
+import {
+  ScheduleContainer,
+  ChooseWeek,
+  Whitespace,
+  ScheduleWrapper,
+  ScheduleError,
+  Arrows,
+  CustomCircularProgress
+} from './styles';
 import RightArrow from '../../assets/svg/right-arrow.svg';
 import LeftArrow from '../../assets/svg/left-arrow.svg';
 import { useTranslation } from 'react-i18next';
 import ScheduleContent from './schedule-content';
 import { useGetWeeklyScheduleQuery } from '../../__data__/services/api/schedule/schedule.api';
 import useWeekNumberMonthYear from '../../utils/schedule/useMonthandWeek';
+import { useSelector } from 'react-redux';
 
 const ScheduleGrid = (): JSX.Element => {
   const { t } = useTranslation();
   const { weekNumber, monthName, year } = useWeekNumberMonthYear();
-  const { data } = useGetWeeklyScheduleQuery(weekNumber);
+  const [currentWeek, setCurrentWeek] = useState(weekNumber);
+  const { data, isFetching, error } = useGetWeeklyScheduleQuery(currentWeek);
+
+  const inputFilterGroup = useSelector((state: any) => state.filters.inputFilterGroup);
+  const inputFilterTeacher = useSelector((state: any) => state.filters.inputFilterTeacher);
+
+  const handleClickPrevWeek = () => {
+    if (currentWeek <= 0) {
+      return;
+    }
+    setCurrentWeek(currentWeek - 1);
+  };
+
+  const handleClickNextWeek = () => {
+    if (currentWeek >= 52) {
+      return;
+    }
+    setCurrentWeek(currentWeek + 1);
+  };
 
   return (
     <ScheduleContainer id='schedule-container'>
       <ScheduleWrapper>
         <ChooseWeek>
-          <img src={LeftArrow} alt={t('schedule:scheduleTranslation.scheduleGrid.backIcon')} />
+          <Arrows
+            src={LeftArrow}
+            alt={t('schedule:scheduleTranslation.scheduleGrid.backIcon')}
+            onClick={handleClickPrevWeek}
+          />
           <Whitespace />
-          <img src={RightArrow} alt={t('schedule:scheduleTranslation.scheduleGrid.forwardIcon')} />
+          <Arrows
+            src={RightArrow}
+            alt={t('schedule:scheduleTranslation.scheduleGrid.forwardIcon')}
+            onClick={handleClickNextWeek}
+          />
           <Whitespace />
-          {monthName} {year} ({weekNumber} {t('schedule:scheduleTranslation.scheduleGrid.week')})
+          {monthName} {year} ({currentWeek} {t('schedule:scheduleTranslation.scheduleGrid.week')})
         </ChooseWeek>
-        {data ? (
+        {isFetching ? (
+          <CustomCircularProgress />
+        ) : data && !error && (inputFilterGroup || inputFilterTeacher) ? (
           <ScheduleContent data={data} />
         ) : (
           <ScheduleError>{t('schedule:scheduleTranslation.scheduleGrid.error')}</ScheduleError>
